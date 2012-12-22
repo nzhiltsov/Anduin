0a1225f872a117833e63d9e12ebaea57b8f31601
Nikita Zhiltsov <nikita.zhiltsov@gmail.com>
2012-12-22 04:36:41 -0500

    Add the out degree processor

---
+++ src/test/scala/ru/ksu/niimm/cll/anduin/OutDegreeProcessorTest.scala
@@ -0,0 +1,44 @@
+package ru.ksu.niimm.cll.anduin
+
+import org.junit.runner.RunWith
+import org.specs.runner.{JUnit4, JUnitSuiteRunner}
+import org.specs.Specification
+import com.twitter.scalding.{TupleConversions, Tsv, TextLine, JobTest}
+import util.NodeParser._
+
+/**
+ * @author Nikita Zhiltsov 
+ */
+@RunWith(classOf[JUnitSuiteRunner])
+class OutDegreeProcessorTest extends JUnit4(OutDegreeProcessorTestSpec)
+
+object OutDegreeProcessorTestSpec extends Specification with TupleConversions {
+  "Out degree processor" should {
+    JobTest("ru.ksu.niimm.cll.anduin.OutDegreeProcessor").
+      arg("input", "inputFile").
+      arg("output", "outputFile").
+      source(TextLine("inputFile"), List(
+      // 1st row
+      ("0", "<http://eprints.rkbexplorer.com/id/caltech/eprints-7519> " +
+        "<http://www.aktors.org/ontology/portal#has-author> <http://eprints.rkbexplorer.com/id/caltech/person-1> " +
+        "<http://somecontext.com/1> ."),
+      // 2nd row
+      ("1", "<http://eprints.rkbexplorer.com/id/caltech/person-1> " +
+        "<http://www.aktors.org/ontology/portal#knows> <http://eprints.rkbexplorer.com/id/caltech/person-2> <http://somecontext.com/1> ."),
+      // 3rd row
+      ("2", "<http://eprints.rkbexplorer.com/id/caltech/person-1> " +
+        "<http://www.aktors.org/ontology/portal#label> \"No. 1 RNA researcher\" <http://somecontext.com/1> .")
+    )).
+      sink[(Subject, Int)](Tsv("outputFile")) {
+      outputBuffer =>
+        "output correct ntuples" in {
+          outputBuffer.size must_== 2
+          outputBuffer(1)._1 mustEqual "<http://eprints.rkbexplorer.com/id/caltech/eprints-7519>"
+          outputBuffer(1)._2 must_== 1
+          outputBuffer(0)._1 mustEqual "<http://eprints.rkbexplorer.com/id/caltech/person-1>"
+          outputBuffer(0)._2 must_== 2
+        }
+    }.run.
+      finish
+  }
+}
