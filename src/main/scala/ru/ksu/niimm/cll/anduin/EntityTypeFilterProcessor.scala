4541d20b878c6ac8527950934201024d663a0e4c
Nikita Zhiltsov <nikita.zhiltsov@gmail.com>
2013-02-01 14:48:40 -0500

    Add a processor to filter entity type info by given entities

---
+++ src/main/scala/ru/ksu/niimm/cll/anduin/EntityTypeFilterProcessor.scala
@@ -0,0 +1,29 @@
+package ru.ksu.niimm.cll.anduin
+
+import com.twitter.scalding.{Tsv, Job, Args, TypedTsv}
+
+/**
+ * Given a list of entities and list of types for all entities,
+ * this processor outputs the list of entity types only for the given entities
+ *
+ * @author Nikita Zhiltsov 
+ */
+class EntityTypeFilterProcessor(args: Args) extends Job(args) {
+  private val relevantEntities =
+    TypedTsv[(Int, String)](args("inputEntityList")).read.rename((0, 1) ->('relEntityId, 'relEntityURI))
+
+  private val entityWithTypes = TypedTsv[(String, String)](args("input")).read.rename((0, 1) ->('entityURI, 'types))
+
+  relevantEntities.joinWithLarger('relEntityURI -> 'entityURI, entityWithTypes)
+    .project(('relEntityId, 'relEntityURI, 'types))
+    .filter('relEntityId) {
+    // todo: a workaround, don't realize why there are empty entries after inner join
+    field: String =>
+      field != null &&
+        !field.trim.isEmpty
+  }
+    .groupAll {
+    _.sortBy('relEntityId)
+  }
+    .write(Tsv(args("output")))
+}
---
+++ src/test/scala/ru/ksu/niimm/cll/anduin/EntityTypeFilterProcessorTest.scala
@@ -0,0 +1,47 @@
+package ru.ksu.niimm.cll.anduin
+
+import org.specs.runner.JUnit4
+import org.specs.Specification
+import com.twitter.scalding.{Tsv, TypedTsv, JobTest, TupleConversions}
+
+/**
+ * @author Nikita Zhiltsov 
+ */
+class EntityTypeFilterProcessorTest extends JUnit4(EntityTypeFilterProcessorSpec)
+
+object EntityTypeFilterProcessorSpec extends Specification with TupleConversions {
+  "Entity processor " should {
+    JobTest("ru.ksu.niimm.cll.anduin.EntityTypeFilterProcessor").
+      arg("input", "inputFile").
+      arg("inputEntityList", "inputEntityListFile").
+      arg("output", "outputFile").
+      source(TypedTsv[(Int, String)]("inputEntityListFile"), List(
+      // 1st row
+      (0, "<http://example.org/2>"),
+      // 2nd row
+      (1, "<http://example.org/3>")
+    )).
+      source(TypedTsv[(String, String)]("inputFile"), List(
+      // 1st row
+      ("<http://example.org/1>", "1,2"),
+      // 2nd row
+      ("<http://example.org/2>", "2"),
+      // 3rd row
+      ("<http://example.org/3>", "4,5")
+    )).
+      sink[(Int, String, String)](Tsv("outputFile")) {
+      outputBuffer =>
+        "output correct entities with types" in {
+          outputBuffer.size must_== 2
+          outputBuffer(0)._1 must_== 0
+          outputBuffer(0)._2 mustEqual "<http://example.org/2>"
+          outputBuffer(0)._3 mustEqual "2"
+
+          outputBuffer(1)._1 must_== 1
+          outputBuffer(1)._2 mustEqual "<http://example.org/3>"
+          outputBuffer(1)._3 mustEqual "4,5"
+        }
+    }.run.
+      finish
+  }
+}
