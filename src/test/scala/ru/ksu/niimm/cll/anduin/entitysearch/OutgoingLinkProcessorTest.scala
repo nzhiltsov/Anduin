afa2b1b941713f5fe4c29e79e96c30be1d07f655
Nikita Zhiltsov <nikita.zhiltsov@gmail.com>
2014-12-19 15:59:05 -0500

    Refactor the test

--- src/test/scala/ru/ksu/niimm/cll/anduin/entitysearch/OutgoingLinkProcessorTest.scala
+++ src/test/scala/ru/ksu/niimm/cll/anduin/entitysearch/OutgoingLinkProcessorTest.scala
@@ -5,8 +5,6 @@ import org.specs.runner.{JUnit4, JUnitSuiteRunner}
 import org.specs.Specification
 import com.twitter.scalding._
 import ru.ksu.niimm.cll.anduin.util.NodeParser._
-import com.twitter.scalding.Tsv
-import ru.ksu.niimm.cll.anduin.util.FixedPathLzoTextLine
 
 /**
  * @author Nikita Zhiltsov 
@@ -14,7 +12,7 @@ import ru.ksu.niimm.cll.anduin.util.FixedPathLzoTextLine
 @RunWith(classOf[JUnitSuiteRunner])
 class OutgoingLinkProcessorTest extends JUnit4(OutgoingLinkProcessorTestSpec)
 
-object OutgoingLinkProcessorTestSpec extends Specification with TupleConversions {
+object OutgoingLinkProcessorTestSpec extends Specification {
   "The outgoing link processor job" should {
     JobTest("ru.ksu.niimm.cll.anduin.entitysearch.OutgoingLinkProcessor").
       arg("inputFirstLevel", "inputFirstLevelFile").
@@ -23,29 +21,23 @@ object OutgoingLinkProcessorTestSpec extends Specification with TupleConversions
       source(TypedTsv[(String, Subject, ru.ksu.niimm.cll.anduin.util.NodeParser.Range)]("inputSecondLevelFile"), List(
       ("0", "<http://eprints.rkbexplorer.com/id/caltech/person-1>", "\"No. 1 RNA researcher 1\""),
       ("1", "<http://eprints.rkbexplorer.com/id/caltech/person-2>", "\"<body><p>123</p></body>\""),
-      ("0", "<http://dbpedia.org/resource/Caldwell_High_School_(Caldwell,_Texas)>", "\"Relevant name\""),
-      ("0", "<http://dbpedia.org/resource/Caldwell_High_School_(Caldwell,_Texas)>", "\"Other relevant name\"")))
-      .source(new FixedPathLzoTextLine("inputFirstLevelFile"), List(
+      ("0", "<http://dbpedia.org/resource/Caldwell_High_School_(Caldwell,_Texas)>", "\"Relevant name\"")))
+      .source(TypedTsv[(String, Subject, ru.ksu.niimm.cll.anduin.util.NodeParser.Range)]("inputFirstLevelFile"), List(
       // 1st row
-      ("0", "<http://eprints.rkbexplorer.com/id/caltech/eprints-7519> " +
-        "<http://www.aktors.org/ontology/portal#has-author> <http://eprints.rkbexplorer.com/id/caltech/person-1> ."),
+      ("8", "<http://eprints.rkbexplorer.com/id/caltech/eprints-7519>", "<http://eprints.rkbexplorer.com/id/caltech/person-1>"),
       // 2nd row
-      ("1", "<http://eprints.rkbexplorer.com/id/caltech/person-1> " +
-        "<http://www.aktors.org/ontology/portal#knows> <http://eprints.rkbexplorer.com/id/caltech/person-2> ."),
+      ("10", "<http://eprints.rkbexplorer.com/id/caltech/person-1>", "<http://eprints.rkbexplorer.com/id/caltech/person-2>"),
       // 4th row
-      ("2", "<http://eprints.rkbexplorer.com/id/caltech/person-2> " +
-        "<http://www.aktors.org/ontology/portal#redirect> <http://dbpedia.org/resource/Caldwell_High_School_(Caldwell,_Texas)> ."),
+      ("12", "<http://eprints.rkbexplorer.com/id/caltech/person-2>", "<http://dbpedia.org/resource/Caldwell_High_School_(Caldwell,_Texas)>"),
       // 5th row
-      ("3", "<http://eprints.rkbexplorer.com/id/caltech/person-2> <http://www.aktors.org/ontology/portal#label> \"Person 2\" .")
+      ("13", "<http://eprints.rkbexplorer.com/id/caltech/person-2>", "\"Person 2\"")
     )).
       sink[(Int, Subject, ru.ksu.niimm.cll.anduin.util.NodeParser.Range)](Tsv("outputFile")) {
       outputBuffer =>
         "output the correct entity descriptions" in {
-          outputBuffer.size must_== 4
-          outputBuffer mustContain(2, "<http://eprints.rkbexplorer.com/id/caltech/eprints-7519>", "\"No. 1 RNA researcher 1\"")
-          outputBuffer mustContain(2, "<http://eprints.rkbexplorer.com/id/caltech/person-2>", "\"Relevant name\"")
-          outputBuffer mustContain(2, "<http://eprints.rkbexplorer.com/id/caltech/person-2>", "\"Other relevant name\"")
-          outputBuffer mustContain(2, "<http://eprints.rkbexplorer.com/id/caltech/person-1>", "\"person\"")
+          outputBuffer.size must_== 2
+          outputBuffer mustContain(1, "<http://eprints.rkbexplorer.com/id/caltech/eprints-7519>", "\"No. 1 RNA researcher 1\"")
+          outputBuffer mustContain(1, "<http://eprints.rkbexplorer.com/id/caltech/person-2>", "\"Relevant name\"")
         }
     }.run.
       finish
