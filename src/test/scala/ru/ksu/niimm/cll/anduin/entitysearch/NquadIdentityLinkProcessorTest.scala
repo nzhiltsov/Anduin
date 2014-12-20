eeb6b7b35e14f5ab95b24492ae1d31aac43c9d83
Nikita Zhiltsov <nikita.zhiltsov@gmail.com>
2014-12-20 12:49:31 -0500

    Fix the identity link processor

--- src/test/scala/ru/ksu/niimm/cll/anduin/entitysearch/NquadIdentityLinkProcessorTest.scala
+++ src/test/scala/ru/ksu/niimm/cll/anduin/entitysearch/NquadIdentityLinkProcessorTest.scala
@@ -4,9 +4,8 @@ import org.specs.runner.JUnit4
 import org.specs.Specification
 import com.twitter.scalding._
 import ru.ksu.niimm.cll.anduin.util.NodeParser._
-import com.twitter.scalding.Tsv
-import ru.ksu.niimm.cll.anduin.util.FixedPathLzoTextLine
-
+import com.twitter.scalding.TextLine
+import ru.ksu.niimm.cll.anduin.util.PredicateGroupCodes._
 /**
  * @author Nikita Zhiltsov 
  */
@@ -15,56 +14,59 @@ class NquadIdentityLinkProcessorTest extends JUnit4(NquadIdentityLinkProcessorTe
 object NquadIdentityLinkProcessorTestSpec extends Specification with TupleConversions {
   "The identity link processor job" should {
     JobTest("ru.ksu.niimm.cll.anduin.entitysearch.IdentityLinkProcessor").
-      arg("inputEntityAttributes", "inputEntityAttributesFile").
-      arg("inputGraph", "inputGraphFile").
+      arg("input", "inputFile").
       arg("inputFormat", "nquad").
+      arg("entityNames", "entityNamesFile").
       arg("output", "outputFile")
-      .source(TypedTsv[(Int, Subject, ru.ksu.niimm.cll.anduin.util.NodeParser.Range)]("inputEntityAttributesFile"), List(
-      (0, "<http://eprints.rkbexplorer.com/id/caltech/person-1>", "\"Primary person 1 name\""),
-      (1, "<http://eprints.rkbexplorer.com/id/caltech/person-1>", "\"Other attributes of person 1\""),
-      (0, "<http://eprints.rkbexplorer.com/id/caltech/person-2>", "\"Primary person 2 name\""),
-      (1, "<http://eprints.rkbexplorer.com/id/caltech/person-2>", "\"Other attributes of person 2\""),
-      (0, "<http://eprints.rkbexplorer.com/id/caltech/person-3>", "\"Primary person 3 name\""),
-      (1, "<http://eprints.rkbexplorer.com/id/caltech/person-5>", "\"Other attributes of person 5\""),
-      (1, "<http://eprints.rkbexplorer.com/id/caltech/person-6>", "\"Other attributes of person 6\"")
+      .source(TypedTsv[(String, String)]("entityNamesFile"), List(
+      ("<http://eprints.rkbexplorer.com/id/caltech/person-1>", "\"person 1\""),
+      ("<http://eprints.rkbexplorer.com/id/caltech/person-2>", "\"person 2\""),
+      ("<http://eprints.rkbexplorer.com/id/caltech/person-3>", "\"person 3\""),
+      ("<http://eprints.rkbexplorer.com/id/caltech/person-6>", "\"person 6\"")
     ))
-      .source(new FixedPathLzoTextLine("inputGraphFile"), List(
+      .source(TextLine("inputFile"), List(
       // 1st row
       ("0", "<http://eprints.rkbexplorer.com/id/caltech/eprints-7519> <http://www.aktors.org/ontology/portal#has-author>" +
-        " <http://eprints.rkbexplorer.com/id/caltech/person-1> <http://somecontext.com/1> ."),
+        " <http://eprints.rkbexplorer.com/id/caltech/person-1> <http://context.com/1> ."),
       // 2nd row
       ("1", "<http://eprints.rkbexplorer.com/id/caltech/person-1> <http://www.w3.org/2002/07/owl#sameAs> " +
-        "<http://eprints.rkbexplorer.com/id/caltech/person-2> <http://somecontext.com/1> ."),
+        "<http://eprints.rkbexplorer.com/id/caltech/person-2> <http://context.com/1> ."),
       // 3d row
       ("2", "<http://eprints.rkbexplorer.com/id/caltech/person-3> <http://www.w3.org/2002/07/owl#sameAs> " +
-        "<http://eprints.rkbexplorer.com/id/caltech/person-1> <http://somecontext.com/1> ."),
+        "<http://eprints.rkbexplorer.com/id/caltech/person-1> <http://context.com/1> ."),
       // 4th row
       ("3", "<http://eprints.rkbexplorer.com/id/caltech/person-1> <http://www.w3.org/2002/07/owl#sameAs> " +
-        "<http://eprints.rkbexplorer.com/id/caltech/person-4> <http://somecontext.com/1> ."),
+        "<http://eprints.rkbexplorer.com/id/caltech/person-4> <http://context.com/1> ."),
       // 5th row
-      ("4", "<http://eprints.rkbexplorer.com/id/caltech/person-5> <http://dbpedia.org/property/disambiguates> " +
-          "<http://eprints.rkbexplorer.com/id/caltech/person-1> <http://somecontext.com/1> ."),
+      ("4", "<http://eprints.rkbexplorer.com/id/caltech/person-5> <http://dbpedia.org/ontology/wikiPageDisambiguates> " +
+        "<http://eprints.rkbexplorer.com/id/caltech/person-1> <http://context.com/1> ."),
       // 6th row
-      ("5", "<http://eprints.rkbexplorer.com/id/caltech/person-1> <http://dbpedia.org/property/redirect> " +
-        "<http://eprints.rkbexplorer.com/id/caltech/person-6> <http://somecontext.com/1> .")
+      ("5", "<http://eprints.rkbexplorer.com/id/caltech/person-6> <http://dbpedia.org/ontology/wikiPageRedirects> " +
+        "<http://eprints.rkbexplorer.com/id/caltech/person-1> <http://context.com/1> .")
     )).
       sink[(Int, Subject, ru.ksu.niimm.cll.anduin.util.NodeParser.Range)](Tsv("outputFile")) {
       outputBuffer =>
         "output the correct entity descriptions" in {
-          outputBuffer.size must_== 11
-          outputBuffer mustContain(2, "<http://eprints.rkbexplorer.com/id/caltech/person-1>", "\"Primary person 2 name\"")
-          outputBuffer mustContain(3, "<http://eprints.rkbexplorer.com/id/caltech/person-1>", "\"Other attributes of person 2\"")
-          outputBuffer mustContain(3, "<http://eprints.rkbexplorer.com/id/caltech/person-1>", "\"Other attributes of person 5\"")
-          outputBuffer mustContain(3, "<http://eprints.rkbexplorer.com/id/caltech/person-1>", "\"Other attributes of person 6\"")
-          outputBuffer mustContain(2, "<http://eprints.rkbexplorer.com/id/caltech/person-1>", "\"Primary person 3 name\"")
-          outputBuffer mustContain(2, "<http://eprints.rkbexplorer.com/id/caltech/person-3>", "\"Primary person 1 name\"")
-          outputBuffer mustContain(3, "<http://eprints.rkbexplorer.com/id/caltech/person-3>", "\"Other attributes of person 1\"")
-          outputBuffer mustContain(2, "<http://eprints.rkbexplorer.com/id/caltech/person-2>", "\"Primary person 1 name\"")
-          outputBuffer mustContain(3, "<http://eprints.rkbexplorer.com/id/caltech/person-2>", "\"Other attributes of person 1\"")
-          outputBuffer mustContain(2, "<http://eprints.rkbexplorer.com/id/caltech/person-4>", "\"Primary person 1 name\"")
-          outputBuffer mustContain(3, "<http://eprints.rkbexplorer.com/id/caltech/person-4>", "\"Other attributes of person 1\"")
+          outputBuffer.size must_== 5
+          //          outputBuffer mustContain(SIMILAR_ENTITY_NAMES,
+          //            "<http://eprints.rkbexplorer.com/id/caltech/person-1>", "\"person 3\" \"person 2\" \"person 6\"")
+          outputBuffer mustContain(SIMILAR_ENTITY_NAMES,
+            "<http://eprints.rkbexplorer.com/id/caltech/person-3>", "\"person 1\"")
+          outputBuffer mustContain(SIMILAR_ENTITY_NAMES,
+            "<http://eprints.rkbexplorer.com/id/caltech/person-2>", "\"person 1\"")
+          outputBuffer mustContain(SIMILAR_ENTITY_NAMES,
+            "<http://eprints.rkbexplorer.com/id/caltech/person-4>", "\"person 1\"")
+          outputBuffer mustContain(SIMILAR_ENTITY_NAMES,
+            "<http://eprints.rkbexplorer.com/id/caltech/person-5>", "\"person 1\"")
+
+          outputBuffer mustExist(isValidTuple)
         }
     }.run.
       finish
   }
+
+  def isValidTuple: ((Int, Subject, ru.ksu.niimm.cll.anduin.util.NodeParser.Range)) => Boolean = tuple =>
+    tuple._1 == SIMILAR_ENTITY_NAMES &&
+      tuple._2.equals("<http://eprints.rkbexplorer.com/id/caltech/person-1>") && tuple._3.split("\"").contains("person 3") &&
+      tuple._3.split("\"").contains("person 2") && tuple._3.split("\"").contains("person 6")
 }
